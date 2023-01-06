5135031f7081d63328b4f6325a36450f38e471f3
Vincent <113322585+PLM-Vincent@users.noreply.github.com>
2023-01-06 18:55:18 +0800

    Added smooth scroll using scrollspy

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,18 @@
         });
         wow.init();
 
+        $("body").scrollspy({
+            target: ".navbar-collapse",
+            offset: 200
+        });
+        $('a.smooth-menu').on('click', function(event) {
+            var $anchor = $(this);
+            var headerH = '75';
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - headerH + "px"
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
 
     }); 
 })(jQuery); 
